54768f158f168ef1d2b760423638700ffcaf90d5
yanghaohsiang <sseepers7777@gmail.com>
2026-04-29 18:58:52 +0800

    Initial Commit

---
+++ SmartExpenseTracker/SmartExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartExpenseTrackerApp.swift
+//  SmartExpenseTracker
+//
+//  Created by 楊皓翔 on 2026/4/29.
+//
+
+import SwiftUI
+
+@main
+struct SmartExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartExpenseTrackerTests/SmartExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartExpenseTrackerTests.swift
+//  SmartExpenseTrackerTests
+//
+//  Created by 楊皓翔 on 2026/4/29.
+//
+
+import Testing
+@testable import SmartExpenseTracker
+
+struct SmartExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
